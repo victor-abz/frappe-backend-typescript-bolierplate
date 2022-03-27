702928cafbfca4815921861f8fb6e8e696571d06
Abizeyimana Victor <svicky.shema@gmail.com>
2022-03-27 10:51:36 +0200

    Fix Middlewares

--- src/middleware/sample.middleware.ts
+++ src/middleware/sample.middleware.ts
@@ -3,7 +3,7 @@ import { NextFunction, Request, Response } from 'express';
 export const testMid = (number: number) => {
 	return function (req: Request, res: Response, next: NextFunction) {
 		if (number > 10) {
-			res.status(404).json({ message: 'Error found' });
+			return res.status(404).json({ message: 'Error found' });
 		}
 		next();
 	};
